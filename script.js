48e1dca58a408c316c854f78037ec9d3782dd4bc
jahardesty <81397442+jahardesty@users.noreply.github.com>
2024-02-21 20:07:04 -0500

    next prev buttons work

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 "use strict";
+const btnPrevious = document.getElementById("btnPrevious");
+const btnNext = document.getElementById("btnNext");
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
@@ -15,6 +17,27 @@ const closeModal = function () {
   overlay.classList.add("hidden");
 };
 
+const imageFiles = ["acorient.png", "leviathan.jpg"];
+let currentIndexImage = 0;
+
+// function to change the image
+function changeBook() {
+  const imageEl = document.getElementById("image");
+  imageEl.src = imageFiles[currentIndexImage];
+}
+// function incorporating the changeBook function to go back to previous image
+function prevImage() {
+  currentIndexImage =
+    (currentIndexImage - 1 + imageFiles.length) % imageFiles.length;
+  changeBook();
+}
+// function incorporating the changeBook function to go to next image
+function nextImage() {
+  currentIndexImage =
+    (currentIndexImage + 1 + imageFiles.length) % imageFiles.length;
+  changeBook();
+}
+
 for (let i = 0; i < btnShowModal.length; i++)
   btnShowModal[i].addEventListener("click", openModal);
 
@@ -26,3 +49,5 @@ document.addEventListener("keydown", function (e) {
     closeModal();
   }
 });
+
+changeBook();
